6f7271b6fe618795f4e4250a1d39dc516ef1b439
Abhishekkugali <126229384+Abhishekkugali@users.noreply.github.com>
2023-03-06 14:27:04 +0530

    Add files via upload

---
+++ App2.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import "./app2.css";
+
+const App2=()=>{
+    return(
+        <div id="ii">
+        <img   src="https://play-lh.googleusercontent.com/DR77tjTRELjjz08osmWgvIFyUvkbQItRf0II8r-eaIzLDOH9YGxSIfIrcToj3IOriyc" alt="" />
+        <div> <input id="bb" type={"text"} name=""   placeholder=" Cricbuzz plus "/></div>
+        <div className="menuBlock">
+            <ul>
+                <li><a href="#a">Live Scores</a></li>
+                <li><a href="#s">Schedule   </a></li>
+                <li><a href="#c">Archives   </a></li>
+                <li><a href="#v">News       </a></li>
+                <li><a href="#r">Series     </a></li>
+                <li><a href="#t">Teams      </a></li>
+                <li><a href="#n">Videos     </a></li>
+                <li><a href="#j">Rankings   </a></li>
+                <li><a href="#q">More       </a></li>
+                </ul>
+            </div>
+
+        </div>
+    )
+    }
+export default App2;
